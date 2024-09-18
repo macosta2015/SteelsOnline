85d5132969a1d72240f2782af30f76a47dd1d4f2
macosta2015 <macosta2015@my.fit.edu>
2024-09-17 23:20:36 -0400

    Editing code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HelperTextAligned from './components/form';
 
@@ -9,11 +8,11 @@ import HelperTextAligned from './components/form';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <HelperTextAligned />
     <HelperTextAligned />
 
   </React.StrictMode>
 );
+
 
 reportWebVitals();
