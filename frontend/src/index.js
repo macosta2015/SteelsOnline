1e54bf4745c41eb5af2faa5e6fc856dce06cc008
Mario Acosta <macosta2015@my.fit.edu>
2024-09-19 09:29:53 -0400

    commit code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import HelperTextAligned from './components/form';
-
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelperTextAligned />
-    <HelperTextAligned />
+    <App />
 
   </React.StrictMode>
 );
