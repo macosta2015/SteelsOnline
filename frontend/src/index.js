f7fc8f0b05fd0aada2b343eeaa23541d0427ad07
Mario Acosta <macosta2015@my.fit.edu>
2024-09-21 18:51:47 -0400

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -38,36 +38,3 @@ root.render(
 
 reportWebVitals();
 
-
-// import React, { useState } from 'react'; // Make sure useState is imported
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import reportWebVitals from './reportWebVitals';
-// import EmailList from './components/email/email.js';
-// import DeleteEmail from './components/email/deleteemail.js';
-// import DisplayEmails from './components/email/displayemail.js';
-
-// import App from './App.js'
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// function Main() {
-//   const [emailList, setEmailList] = useState([]);
-
-//   return (
-//     <>
-//       <App />
-//       <EmailList emailList={emailList} setEmailList={setEmailList} />
-//       <DisplayEmails />
-//     </>
-//   );
-// }
-
-// root.render(
-//   <React.StrictMode>
-//     <Main />
-//   </React.StrictMode>
-// );
-
-
-
-// reportWebVitals();
