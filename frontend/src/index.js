10a6a3eaeb0bfb72ab7e6778a3db3fa47cb3c265
Mario Acosta <macosta2015@my.fit.edu>
2024-09-21 12:30:34 -0400

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import HelperTextAligned from './components/form/form.js';
-import App from './App'
+import App from './App.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
